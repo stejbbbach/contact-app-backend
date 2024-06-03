ac263703bfae5cfc886f6d399eaee13c76b9682d
stejbi <bartosz.stejbach@gmail.com>
2024-06-04 00:18:43 +0200

    configure basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('test');
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+//app
+const app = express()
+const port = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+
+//routes
+
+//mongo
+
+//server start
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
